d53d698e5714f17264e44eb6784ef1e71042c42b
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-07 17:44:47 -0500

    Create setup.bash

---
+++ setup.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup a folder to run the pipeline
+
+dt=$(date -d "today" +"%Y_%m_%d_%H_%M")
+jobsDir=jobs-$dt
+
+git clone https://github.com/NicholasSynovic/ssl-metrics-pipeline $jobsDir
+
+cd $jobsDir
+
+python3.10 -m venv env
+source env/bin/activate
+pip install ssl-metrics-meta
