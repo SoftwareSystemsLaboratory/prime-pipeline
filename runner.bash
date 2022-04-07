c0712711afa9c9b69b1de66a500c2adac141bcb6
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-07 08:48:48 -0500

    Timestamp files

--- runner.bash
+++ runner.bash
@@ -7,8 +7,8 @@ token=$2
 repositoryFolder=$(basename $1 .git)
 githubShortCode=$(echo $1 | sed -e 's/https:\/\/github.com\///g')
 
-jsonDir=json_$dt
-graphsDir=graphs_$dt
+jsonDir=json-$dt
+graphsDir=graphs-$dt
 
 mkdir $jsonDir
 mkdir $graphsDir
@@ -20,25 +20,25 @@ git clone $1
 git -C $repositoryFolder pull
 
 # Extract commits
-ssl-metrics-git-commits-loc-extract -d $repositoryFolder -b HEAD -o $jsonDir/commits_$repositoryFolder.json
+ssl-metrics-git-commits-loc-extract -d $repositoryFolder -b HEAD -o $jsonDir/commits_$repositoryFolder-$dt.json
 
 # Extract issues
-ssl-metrics-github-issues-collect -p -r $githubShortCode -o $jsonDir/issues_$repositoryFolder.json -t $token
+ssl-metrics-github-issues-collect -p -r $githubShortCode -o $jsonDir/issues_$repositoryFolder-$dt.json -t $token
 
 # Compute bus factor
-ssl-metrics-git-bus-factor-compute -i $jsonDir/commits_$repositoryFolder.json -o $jsonDir/bf_$repositoryFolder.json
+ssl-metrics-git-bus-factor-compute -i $jsonDir/commits_$repositoryFolder-$dt.json -o $jsonDir/bf_$repositoryFolder-$dt.json
 
 # Graph bus factor
-python graph.py -i $jsonDir/bf_$repositoryFolder.json -o $graphsDir/bf_$repositoryFolder.pdf
+python graph.py -i $jsonDir/bf_$repositoryFolder-$dt.json -o $graphsDir/bf_$repositoryFolder-$dt.pdf
 
 # Compute issue density
-ssl-metrics-github-issue-density-compute -c $jsonDir/commits_$repositoryFolder.json -i $jsonDir/issues_$repositoryFolder.json -o $jsonDir/id_$repositoryFolder.json
+ssl-metrics-github-issue-density-compute -c $jsonDir/commits_$repositoryFolder-$dt.json -i $jsonDir/issues_$repositoryFolder-$dt.json -o $jsonDir/id_$repositoryFolder-$dt.json
 
 # Graph issue density
-python graph.py -i $jsonDir/id_$repositoryFolder.json -o $graphsDir/id_$repositoryFolder.pdf
+python graph.py -i $jsonDir/id_$repositoryFolder-$dt.json -o $graphsDir/id_$repositoryFolder-$dt.pdf
 
 # Compute productivity
-ssl-metrics-git-productivity-compute -i $jsonDir/commits_$repositoryFolder.json -o $jsonDir/p_$repositoryFolder.json
+ssl-metrics-git-productivity-compute -i $jsonDir/commits_$repositoryFolder-$dt.json -o $jsonDir/p_$repositoryFolder-$dt.json
 
 # Graph productivity
-python graph.py -i $jsonDir/p_$repositoryFolder.json -o $graphsDir/p_$repositoryFolder.pdf
+python graph.py -i $jsonDir/p_$repositoryFolder-$dt.json -o $graphsDir/p_$repositoryFolder-$dt.pdf
