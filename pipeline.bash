ffada7ee5b6d2c4c25bd01291fa362727a1a4f47
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-07 17:55:44 -0500

    Work on README.md and make the pipeline more parametric

--- pipeline.bash
+++ pipeline.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This is the executable to run an analysis on multiple repositories at once
 token=$1
+repositoryListFile=$2
 
-cat githubRepositories.txt | parallel -j 5 tmux new-session -d "./runner.bash {} $token"
+cat $repositoryListFile | parallel -j 5 tmux new-session -d "./runner.bash {} $token"
