87f4d1ec55bfd8a74e6996b8b08594e8949829bc
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-04-25 08:28:50 -0500

    Remove -j flag

--- pipeline.bash
+++ pipeline.bash
@@ -3,4 +3,4 @@
 token=$1
 repositoryListFile=$2
 
-cat $repositoryListFile | parallel -j 5 tmux new-session -d "./runner.bash {} $token $repositoryListFile"
+cat $repositoryListFile | parallel tmux new-session -d "./runner.bash {} $token $repositoryListFile"
